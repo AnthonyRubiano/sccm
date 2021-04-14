22f9b8e8810f39bdf6f89ee7cc6bccf02a0d031a
Anthony Rubiano <administrator@anthonyrubiano.com>
2021-04-14 14:43:15 -0400

    Create Client Cache Cleanup Remediation.ps1

---
+++ Configuration Items/Client Cache Cleanup Remediation.ps1
@@ -0,0 +1,8 @@
+$MinDays = 7
+$UIResourceMgr = New-Object -ComObject UIResource.UIResourceMgr
+$Cache = $UIResourceMgr.GetCacheInfo()
+$Cache.GetCacheElements() |
+where-object {[datetime]$_.LastReferenceTime -lt (get-date).adddays(-$mindays)} |
+foreach {
+$Cache.DeleteCacheElement($_.CacheElementID)
+}
